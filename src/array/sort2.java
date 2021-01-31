4a6848e40b54b9c4ff2f1042b266a57d1bbd6d9b
ashrut saxena <ashrut.saxena_cs18@gla.ac.in>
2021-01-31 19:01:39 +0700

    progee

--- src/array/sort2.java
+++ src/array/sort2.java
@@ -2,6 +2,27 @@ package array;
 
 public class sort2 {
     public static void main(String[] args) {
-        
+        int arr[]=new int[]{4,5,8,92,55,66};
+        int temp=0;
+
+        System.out.println("elements of original array is:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+"");
+        }
+
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i+1; j <arr.length ; j++) {
+                if(arr[i]<arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println("elements after sorting is:");
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]+" ");
+
+        }
     }
 }
